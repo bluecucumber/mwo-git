61ee76fa6c68b7463264f59b0549757e94bc610b
Mateusz Grabowski <grabowski.mat@gmail.com>
2017-02-11 10:22:20 +0100

    2nd fakap

--- SomeProgram.java
+++ SomeProgram.java
@@ -3,6 +3,11 @@ public class SomeProgram {
         System.out.println("We are learning to use Git.");
 		System.out.println("We will be Git experts, soon");
 		System.out.println("Yet, we are the experts");
-		System.out.println("Fakap na branczu");
+		
     }
+}
+
+public void manageTheSatellite() {
+    // TODO implement
+	System.out.println("Fakap na branczu");
 }
